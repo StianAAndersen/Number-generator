a635598a8132b1b99a24b4c85e65bea937987f0f
StianAAndersen <stian.andersen@hotmail.no>
2021-11-14 11:33:21 +0100

    fixed indentation error with result

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ class Generator:
             result.append(randint(minRange, maxRange))
 
         
-        return f"Amount retuerned: {amount}\nStarting range: {minRange}\nEndring range: {maxRange}\n Result: {result}"
+        return f"Amount retuerned: {amount}\nStarting range: {minRange}\nEndring range: {maxRange}\nResult: {result}"
 
 
 if (__name__ == '__main__'):
