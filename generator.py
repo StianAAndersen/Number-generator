584736efc7f28b779d7a9297ad5bca403f4e0b30
StianAAndersen <stian.andersen@hotmail.no>
2021-11-14 11:19:18 +0100

    Created a number generator

---
+++ generator.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+class Generator:
+    def randomNumRange(self, amount, minRange, maxRange):
+        result = []
+
+        for i in range(amount):
+            result.append(randint(minRange, maxRange))
+
+        
+        return f"Amount retuerned: {amount}\nStarting range: {minRange}\nEndring range: {maxRange}\nResult: {result}"
+
+
+if (__name__ == '__main__'):
+    gen = Generator()
+    print(gen.randomNumRange(10, 1, 50))
+
+
+
+
+
+
+
+
+
